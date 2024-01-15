3712985341d3faa6e77c704b2cd696dcc2da3986
cjie yan <cj.yan@gmail.com>
2024-01-15 11:53:46 +0800

    格式调整

--- sdk/pkg/response/model.go
+++ sdk/pkg/response/model.go
@@ -2,7 +2,7 @@ package response
 
 type Response struct {
 	// 数据集
-	RequestId string `protobuf:"bytes,1,opt,name=requestId,proto3" json:"requestId,omitempty"`
+	RequestId string `protobuf:"bytes,1,opt,name=requestId,proto3" json:"request_id,omitempty"`
 	Code      int32  `protobuf:"varint,2,opt,name=code,proto3" json:"code,omitempty"`
 	Msg       string `protobuf:"bytes,3,opt,name=msg,proto3" json:"msg,omitempty"`
 	Status    string `protobuf:"bytes,4,opt,name=status,proto3" json:"status,omitempty"`
